fc4380cb2228d9d1a548d6f7588779be3f14ea92
Hyper <DevHypers@github.com>
2020-11-16 16:12:26 +0900

    divide_report

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import discord, asyncio, os, smtplib
 from datetime import date
-from email.mime.text import MIMEText
 
 client = discord.Client()
 
@@ -13,29 +12,6 @@ def get_today():
 def get_yesterday(): 
     yesterday = date.today() - timedelta(1)
     return str(yesterday.strftime('%Y-%m-%d'))
-
-def send_report():
-    # 세션생성, 로그인
-    s = smtplib.SMTP('smtp.gmail.com', 587)
-    s.starttls()
-    s.login(str(os.environ["EMAIL"]), str(os.environ["PASS"]))
-
-    # 제목, 본문 작성
-    msg = MIMEMultipart()
-    msg['Subject'] = get_yesterday() + " Report"
-
-    # 파일첨부 (파일 미첨부시 생략가능)
-    attachment = open(get_yesterday() + ".txt", 'rb')
-    part = MIMEBase('application', 'octet-stream')
-    part.set_payload((attachment).read())
-    encoders.encode_base64(part)
-    part.add_header('Content-Disposition', "attachment; filename= " + filename)
-    msg.attach(part)
-
-    # 메일 전송
-    s.sendmail(str(os.environ["EMAIL"]), str("REPORT_EMAIL"), msg.as_string())
-    s.quit()
-
 
 async def bt(games):
     await client.wait_until_ready()
